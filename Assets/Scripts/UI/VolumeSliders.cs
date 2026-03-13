1ac9a26ee84f197a07d64706c755f520fc16a7b6
Hana <xyzmisterysolve@gmail.com>
2026-03-13 14:06:34 +0100

    settings are now stored in playerprefs

--- Assets/Scripts/UI/VolumeSliders.cs
+++ Assets/Scripts/UI/VolumeSliders.cs
@@ -1,6 +1,19 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeSliders : MonoBehaviour
 {
-    
+    private PlayerPrefsGestionnary playerPrefs;
+
+    private void Start()
+    {
+        playerPrefs = gameObject.GetComponent<PlayerPrefsGestionnary>();
+    }
+
+    public void ChangeVolume()
+    {
+        playerPrefs.SetPlayerPrefs(0, (int)gameObject.GetComponent<Slider>().value);
+    }
 }
