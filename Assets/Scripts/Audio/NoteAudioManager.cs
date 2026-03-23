165b23e2de04d0320c97606e70dc04f1fa3ee64a
matcodeew <150682957+matcodeew@users.noreply.github.com>
2026-03-23 16:30:52 +0100

    make cheat panel (doestn work now )

--- Assets/Scripts/Audio/NoteAudioManager.cs
+++ Assets/Scripts/Audio/NoteAudioManager.cs
@@ -1,11 +1,23 @@
 using AYellowpaper.SerializedCollections;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class NoteAudioManager : MonoBehaviour
 {
     [Header("Audio clip setter")]
     [SerializeField] SerializedDictionary<NoteID, AudioClip> noteSound = new SerializedDictionary<NoteID, AudioClip>();
     [SerializeField] private AudioSource sources;
+
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private AudioMixerGroup master;
+    [SerializeField] private AudioMixerGroup music;
+    [SerializeField] private AudioMixerGroup sfx;
+
+    [Header("Volume slider ref")]
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
 
     #region Events  
     private void OnEnable()
@@ -23,4 +35,29 @@ public class NoteAudioManager : MonoBehaviour
         sources.clip = noteSound[callback.id];
         sources.Play();
     }
+
+    #region utility
+
+    public void ChangeVolume(float pVolume)
+    {
+        sources.volume = pVolume;
+    }
+    public void ChangeMixerVolume(float pVolume, AudioMixerGroup pMixer)
+    {
+        mixer.SetFloat(pMixer.name, pVolume);
+    }
+    public void ChangeMasterMixerVolume()
+    {
+        mixer.SetFloat(master.name, 0.01f + Mathf.Log10(masterSlider.value) * 20);
+    }
+    public void ChangeMusicMixerVolume()
+    {
+        mixer.SetFloat(music.name, 0.01f + Mathf.Log10(musicSlider.value) * 20);
+    }
+    public void ChangeSfxMixerVolume()
+    {
+        mixer.SetFloat(sfx.name, Mathf.Log10(sfxSlider.value) * 20);
+    }
+
+    #endregion
 }
